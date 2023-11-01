0f2c7c13dce9dd4e969e847a18f3759085d02344
fryniewi <01178583@pw.edu.pl>
2023-11-01 14:08:30 +0100

    slightly expanded the report

--- lab01/plotting.py
+++ lab01/plotting.py
@@ -14,3 +14,13 @@ def plot(learningRate, problem, problemGradient, x0, steps=10, FinalStep=1e-5):
     s = MySolver(learningRate)
     s.solve(problem, problemGradient, x0, stepLimit=steps, finalStep=FinalStep)
     plt.plot(s.valueArray, label=str(learningRate))
+
+
+def barChart(keys, values, title='', ylabel='', xlabel=''):
+    newKeys = []
+    for key in keys:
+        newKeys.append(str(key))
+    plt.title(title)
+    plt.ylabel(ylabel)
+    plt.xlabel(xlabel)
+    plt.bar(newKeys, values, width=0.4)
