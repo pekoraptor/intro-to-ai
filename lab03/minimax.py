cedec3010fe41d15c3d76514d6d3e6e977ceb9a3
fryniewi <01178583@pw.edu.pl>
2023-11-27 12:38:55 +0100

    started working on minimax and the heuristic function for connect four

---
+++ lab03/minimax.py
@@ -0,0 +1,48 @@
+from two_player_games.games.connect_four import ConnectFour, ConnectFourMove, ConnectFourState
+import math
+import random
+import copy
+
+
+def connectFourHeuristic(node, maximizingPlayer):
+    winner = node.get_winner()
+    if winner:
+        if winner == maximizingPlayer:
+            return math.inf
+        else:
+            return -math.inf
+
+
+def minimax(node, depth, maximizingPlayer, heuristic):
+    if depth == 0 or node.is_finished():
+        return heuristic(node), None
+
+    bestMoves = []
+
+    if node.get_current_player() == maximizingPlayer:
+        value = -math.inf
+        chosenMove = None
+        for move in node.get_moves():
+            newNode = copy.deepcopy(node)
+            newNode.make_move(move)
+            newVal, = minimax(newNode, depth - 1, maximizingPlayer, heuristic)
+            if newVal > value:
+                value = newVal
+                bestMoves = [move]
+            elif newVal == value:
+                bestMoves.append(move)
+        return value, random.choice(bestMoves)
+
+    else:
+        value = +math.inf
+        chosenMove = None
+        for move in node.get_moves():
+            newNode = copy.deepcopy(node)
+            newNode.make_move(move)
+            newVal, = minimax(newNode, depth - 1, maximizingPlayer, heuristic)
+            if newVal < value:
+                value = newVal
+                bestMoves = [move]
+            elif newVal == value:
+                bestMoves.append(move)
+        return value, random.choice(bestMoves)
